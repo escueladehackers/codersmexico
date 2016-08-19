a8374a0fbd5651261e579509d8002f931d52abe7
poguez <zeugop@gmail.com>
2016-08-18 20:12:56 -0500

    Se agregan anotaciones de los endpoints de la aplicación

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,17 @@
 class ProfilesController < ActionController::Base
   layout "application"
 
-
+  # GET /profiles/
   def index
     @users = User.all
   end
 
+  # GET    /profiles/:id
   def show
     @user = User.find(params[:id])
   end
 
+  # GET /profiles/:id/edit
   def edit
     redirect_to edit_user_registration_path(id: params[:id])
   end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,16 +1,21 @@
 class SkillsController < ApplicationController
+
+  # GET /skills
   def index
     @skills = Skill.all
   end
 
+  # GET /skill/:id
   def show
     @skill = Skill.find(params[:id])
   end
 
+  # GET /skills/new
   def new
     @skill = Skill.new
   end
 
+  # POST /skills
   def create
     @skill = Skill.new(skill_params)
     if @skill.save
@@ -18,6 +23,7 @@ class SkillsController < ApplicationController
     end
   end
 
+  # PUT/PATCH /skills/:id
   def update
     @skill = Skill.find(params[:id])
     if @skill.update(skill_params)
@@ -26,16 +32,21 @@ class SkillsController < ApplicationController
 
   end
 
+  # GET /skills/:id/edit
   def edit
     @skill = Skill.find(params[:id])
   end
 
-  def delete
+  # DELETE /skills/:id
+  def destroy
     @skill = Skill.find(params[:id])
+    @skill.delete
   end
 
   private
 
+  # Parámetros sanitizados a usar en el update
+  # y en el create del modelo.
   def skill_params
     params.require(:skill).permit(:name, :description, :image)
   end
